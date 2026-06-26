e7dbbb2c38b86875e7ca6d36d030de65380424f4
JoaoVictorSouzaNunes <joaovictorsousanunessousa@gmail.com>
2026-06-26 19:41:02 -0300

    Introdução do menu formatado.

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "bloom.h"
+#include "hash.h"
+
+void menu() {
+    printf("=================================================\n");
+    printf("Sistema de Verificação de Cadastro de Usuários\n");
+    printf("=================================================\n");
+    printf("[ 1 ] - INSERIR\n");
+    printf("[ 2 ] - CONSULTAR\n");
+    printf("[ 3 ] - EXIBIR ESTATISTICAS\n");
+    printf("[ 4 ] - INSERRIR EM LOTE\n");
+    printf("[ 5 ] - SAIR\n");
+}
