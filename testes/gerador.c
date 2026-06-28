b93bac55aa779550818843dda6dfa0689afb2743
JoaoVictorSouzaNunes <joaovictorsousanunessousa@gmail.com>
2026-06-28 11:14:15 -0300

    Introduzindo gerador de registros

---
+++ testes/gerador.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void gerarArquivo(const char *arq, int num_reg) {
+    FILE *file = fopen(arq, "w");
+    if(!file) {
+        printf("Nao foi possivel gerar o arquivo!\n");
+        return;
+    }
+
+    char usuario[12] = '\0';
+    int i, j;
+    for(i = 0; i < num_reg; i++) {
+        for(j = 0; j < 8; j++) {
+        usuario[j] = 'a' + rand()%26;
+        }
+        for(j = 8; j < 11; j++) {
+            usuario[j] = '0' + rand()%10;
+        }
+        fprintf(file, "%s\n", usuario);
+        }
+    fclose(file);
+}
+
+int main() {
+    srand(time(NULL));
+
+    gerarArquivo("registros1000.txt", 1000);
+    gerarArquivo("registros10000.txt", 10000);
+    gerarArquivo("registros100000.txt", 100000);
+    return 0;
+}
